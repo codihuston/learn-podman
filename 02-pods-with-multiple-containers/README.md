c8ac96bdd31c50edb82320998858468ab2523bd7
codi huston <codi.huston540@gmail.com>
2022-03-06 12:36:00 +0000

    Complete simple pod example

--- 02-pods-with-multiple-containers/README.md
+++ 02-pods-with-multiple-containers/README.md
@@ -5,13 +5,19 @@ differ from Docker.
 
 ## About
 
-
-
 For more information about Pods, run the following commands:
 
 ```bash
 $ podman pod --help
 ```
+
+When creating a pod and attaching it to a network, it is assigned a DNS name.
+The name that it is assigned to is the pod name. For example, if you create
+a pod with the name `my_pod` and attach it to a network `my_network`. Other
+pods created on the same network can query `my_pod` over the network by name.
+
+Annoyance: difficult to get status of containers within a pod.
+
 
 ## References
 
--- 02-pods-with-multiple-containers/pod-setup.sh
+++ 02-pods-with-multiple-containers/pod-setup.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+# create a network
+network=test-net
+podman network create "$network"
+
 # create a pod, you must map all ports that will be exposed at creation time!
-pod=$(podman pod create)
+hostname_test_1="test_1"
+hostname_test_2="test_2"
+
+pod_test_1=$(podman pod create --name "$hostname_test_1" --network "$network" -p 127.0.0.1:8081:80)
+pod_test_2=$(podman pod create --name "$hostname_test_2" --network "$network" -p 127.0.0.1:8082:80)
 
 # list pod
 podman pod ls
 
 # add to the pod
-podman run -dt --pod "$pod" -p 32597:80 quay.io/libpod/alpine_nginx:latest
+test_1_nginx=$(podman run -dt --pod "$pod_test_1" docker.io/library/nginx)
+test_2_nginx=$(podman run -dt --pod "$pod_test_2" docker.io/library/nginx)
 
-# list pod, note containers
+# list pod, note all containers are displayed
 podman pod ls
 
-# create new pod and add to it
-podman run -dt --pod new:nginx -p 32597:80 docker.io/library/nginx
+# or list containers and their pods
+podman ps -a --pod
+
+# list running processes in a pod's containers
+podman pod top "$pod_test_1"
+
+# log a pod
+podman pod logs "$pod_test_1"
+
+# list the pod spec
+podman pod inspect "$pod_test_1"
+
+# verify that nginx container in pod 1 can curl nginx container in pod 2
+echo "A: $test_1_nginx, B: $pod_test_2"
+echo "A: $test_2_nginx, B: $pod_test_1"
+podman exec -it "$test_1_nginx" curl "$hostname_test_1"
+podman exec -it "$test_2_nginx" curl "$hostname_test_2"
+
+podman pod stop "$pod_test_1" && podman pod rm "$pod_test_1"
+podman pod stop "$pod_test_2" && podman pod rm "$pod_test_2"
+podman pod ps
